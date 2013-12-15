9da0dbe922d44c6df8b3fc00ae3b7efb5a4a9d01
ciembor <pub@ciemborowicz.pl>
2013-12-15 03:32:31 +0100

    Spec for homepage index view, it should have publications on the list.

--- spec/views/homepage/index.html.erb_spec.rb
+++ spec/views/homepage/index.html.erb_spec.rb
@@ -5,4 +5,14 @@ describe "homepage/index.html.erb" do
     render
     expect(rendered).to have_selector('ul#publications')
   end
+
+  it 'has publications on the list' do
+    assign(:publications, [
+      stub_model(Publication, content: 'First publication.'),
+      stub_model(Publication, content: 'Second publication.')
+    ])
+    render
+    rendered.should have_content('First publication.')
+    rendered.should have_content('Second publication.')
+  end
 end
