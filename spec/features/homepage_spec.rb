6f867a814bf20f2fe919057cd48e25dfc1dc5f0c
ciembor <pub@ciemborowicz.pl>
2013-12-15 01:14:32 +0100

    Feature spec for publications on the homepage.

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature 'Homepage' do
+  let(:first_publication) { Publication.create(content: 'First publication.') }
+  let(:second_publication) { Publication.create(content: 'Second publication.') }
+
+  describe 'includes list of the newest publications' do
+    before do
+      visit '/'
+    end
+
+    it 'which is rendered' do
+      publications = page.find('#publications')
+      expect(publications).to have_content(first_publication.content)
+      expect(publications).to have_content(second_publication.content)
+    end
+
+    it 'which is ordered from the newest to the oldest publications' do
+      page.should have_tag('ul:nth-child(1)', text: first_publication.content)
+      page.should have_tag('ul:nth-child(2)', text: second_publication.content)
+    end
+  end
+end
