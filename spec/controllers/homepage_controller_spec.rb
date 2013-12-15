56cb7d2e234b22db26933207cf5364b04c864917
ciembor <pub@ciemborowicz.pl>
2013-12-15 02:57:48 +0100

    Spec for assigning publications in homepage#index.

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe HomepageController do
 
   describe "GET 'index'" do
+    let(:publication) { Publication.create(content: 'First publication.') }
+
     it "returns http success" do
       get 'index'
       expect(response).to be_success
+    end
+    it "assigns @publications" do
+      get :index
+      expect(assigns(:publications)).to eq([publication])
     end
   end
 
