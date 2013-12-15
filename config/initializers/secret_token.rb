f98b94d36d1efeba9d191d9a06a2733a7141709a
ciembor <pub@ciemborowicz.pl>
2013-12-15 01:11:01 +0100

    Rails setup, rspec and capybara.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortform::Application.config.secret_token = '48d5af190e0a41078889a6c3a6d797602bd2fc1872a5ace44117db12655d788fa18391ac6d6eedaa8a22146b4e804b249d5f9a57e43e515db9a8176778220721'
