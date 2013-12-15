57a18dc2e51d3065a4fdc355c4b91d7fb08e20d2
ciembor <pub@ciemborowicz.pl>
2013-12-15 03:56:46 +0100

    Repaired selectors in feature tests.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 feature 'Homepage' do
-  let(:first_publication) { Publication.create(content: 'First publication.') }
-  let(:second_publication) { Publication.create(content: 'Second publication.') }
+  let!(:first_publication) { Publication.create(content: 'First publication.') }
+  let!(:second_publication) { Publication.create(content: 'Second publication.') }
 
   describe 'includes list of the newest publications' do
     before do
@@ -16,8 +16,8 @@ feature 'Homepage' do
     end
 
     it 'which is ordered from the newest to the oldest publications' do
-      page.should have_selector('ul:nth-child(1)', text: first_publication.content)
-      page.should have_selector('ul:nth-child(2)', text: second_publication.content)
+      page.should have_selector('#publications > li:nth-child(1)', text: first_publication.content)
+      page.should have_selector('#publications > li:nth-child(2)', text: second_publication.content)
     end
   end
 end
